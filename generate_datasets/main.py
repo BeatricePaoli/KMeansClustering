f6cca97214972f273d03bc4bc6eda298ce8dd992
Beatrice Paoli <beatrice.paoli@magentalab.it>
2022-12-12 17:59:41 +0100

    python type hints

--- generate_datasets/main.py
+++ generate_datasets/main.py
@@ -7,7 +7,7 @@ datasets_directory = "./../datasets/"
 clustering_results_directory = "./../clustering_results/"
 
 
-def generate_2d_clustering_dataset_csv(n_samples, centers, directory):
+def generate_2d_clustering_dataset_csv(n_samples: int, centers: int, directory: str) -> None:
     (Xs, ys) = make_blobs(n_samples=n_samples, n_features=2, centers=centers, center_box=(-15.0, 15.0))
     np.savetxt(
         fname=f'{directory}{n_samples}_points_{centers}_centers.csv',
@@ -20,7 +20,7 @@ def generate_2d_clustering_dataset_csv(n_samples, centers, directory):
     plt.savefig(f'{directory}{n_samples}_points_{centers}_centers.png')
 
 
-def read_from_csv_and_plot(path, delimiter):
+def read_from_csv_and_plot(path: str, delimiter: str) -> None:
     points = np.genfromtxt(fname=path, delimiter=delimiter)
     plt.clf()
     plt.scatter(points[:, 0], points[:, 1], c=points[:, 2])
@@ -30,11 +30,8 @@ def read_from_csv_and_plot(path, delimiter):
 
 
 if __name__ == '__main__':
-    # for i in [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]:
+    # for i in [100000, 200000, 300000, 400000, 500000]:
     #     generate_2d_clustering_dataset_csv(i, 4, datasets_directory)
-
-    # for i in range(5, 25, 5):
-    #     generate_2d_clustering_dataset_csv(100000, i, datasets_directory)
 
     for filename in listdir(clustering_results_directory):
         if filename[-3:] == "csv":
